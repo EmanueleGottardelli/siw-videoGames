03e497a6cf83ecb1229742f3996426f22b09340d
EmanueleGottardelli <128832775+EmanueleGottardelli@users.noreply.github.com>
2025-09-20 15:55:23 +0200

    prova

--- src/main/java/it/uniroma3/siw/SiwVideoGamesApplication.java
+++ src/main/java/it/uniroma3/siw/SiwVideoGamesApplication.java
@@ -1,13 +1,29 @@
 package it.uniroma3.siw;
 
+import java.nio.file.Paths;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SiwVideoGamesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiwVideoGamesApplication.class, args);
+	}
+	
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			String absolutePathToImages = Paths.get("src/main/upload").toAbsolutePath().toString();
+			registry.addResourceHandler("/upload/**")
+					.addResourceLocations("file:" + absolutePathToImages + "/")
+					.setCachePeriod(0);
+		}
 	}
 
 }
